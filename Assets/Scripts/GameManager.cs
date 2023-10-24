8e74bc62cc57030e1b6b91fe9347d0d1e7b7e12d
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-10-24 22:40:29 +1100

    Created gamemanager script
    added a score

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int score = 0;                      // Players score
+    bool isGameOver = false;            // Flag to track the game state 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
