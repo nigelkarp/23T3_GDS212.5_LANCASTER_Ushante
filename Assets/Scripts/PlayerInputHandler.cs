9fa6352713af9dc64e111bb77d6586cd386e6f7b
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-10-25 02:25:55 +1100

    Update PlayerInputHandler.cs
    Added listeners to the yes and no buttons that the game will listen to the clicks and either communicate with the gamemanager or do something (undecided for now)

--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInputHandler : MonoBehaviour
 {
-    // Yes button reference
-    // No button reference
+    [SerializeField] private Button _yesBtn; // Yes button reference
+    [SerializeField] private Button _noBtn;  // No button reference
 
-    // Listen for player input
-    // Listen for button clicks (on the yes and no btns) by adding listeners to them
-
+    // Listen for player input on start
+    private void Start()
+    {
+        // Listen for button clicks (on the yes and no btns) by adding listeners to them
+        _yesBtn.onClick.AddListener(OnYesBtnClick);
+        _noBtn.onClick.AddListener(OnNoBtnClick);
+    }
 
     // Handle Yes input, and communicate this to GM
+    void OnYesBtnClick()
+    {
+        // communicate to the game manager ...
+    }
 
     // Handle No input, and communicate this to GM
+    void OnNoBtnClick()
+    {
+        // communicate to the game manager ...
+    }
 }
